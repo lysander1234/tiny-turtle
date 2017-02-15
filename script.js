d8e674efbe92e0e862881c9f2c699a707e7e9d05
lysander <oforilysander123@gmail.com>
2017-02-15 14:37:35 +0000

    i changed stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+TinyTurtle.apply(window);
+left(90);
+forward(90);
+left(90);
+forward(50);
+left(90);
+forward(90);
+left(90);
+forward(50);
+left(90);
+forward(90);
+right(150);
+forward(50);
+right(60);
+forward(50);
+
